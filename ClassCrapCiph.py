eeee1c36df419ce523045f211d326c11d15d4031
dawson270500 <51200862+dawson270500@users.noreply.github.com>
2020-07-09 16:36:04 +0100

    Class version, I got bored

---
+++ ClassCrapCiph.py
@@ -0,0 +1,39 @@
+import random
+
+def split(word):##simple string splitter
+	return [char for char in word]
+
+class CrapCiph:
+	def ciph(inp):##CIpher input
+		seed = random.randint(100, 99999999)
+
+		letters = split(inp)
+		out = ""
+
+		for x in letters:
+			y = ord(x)
+			out = out + str(y*seed) + "-"
+
+		return (str(out)+str(seed)+"-")
+
+	def unciph(inp):##Uncipher input
+		workon = inp
+
+		nums = []
+		out = ""
+
+		for x in workon:
+			if(x=="-"):
+				nums.append(out)
+				out = ""
+			else:
+				out = out+x
+
+		letters = ""
+		seed = nums[len(nums)-1]
+		nums.remove(seed)
+		seed = int(seed)
+		for x in nums:
+			letters = letters +chr(int(int(x)/seed))
+
+		return letters
